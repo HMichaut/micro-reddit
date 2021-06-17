3245c86e01fea7e7422ba77b5e470f42d9202431
HMichaut <hectormichaut@gmail.com>
2021-06-17 23:38:08 +0200

    Post created and relationship added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, uniqueness: true, length: { minimum: 3 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_200326) do
+ActiveRecord::Schema.define(version: 2021_06_17_212838) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_17_200326) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
